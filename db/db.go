28eb276be09371b9dc55b60bdd5ce92a7dd262c1
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2021-12-31 04:52:46 +0700

    Connect Posgres + Routes

---
+++ db/db.go
@@ -0,0 +1,60 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/nikitamirzani323/go_wl_api/helpers"
+)
+
+var db *sql.DB
+
+func Init() {
+	var conString string = ""
+
+	err := godotenv.Load()
+	if err != nil {
+		panic("Failed to Load env file")
+	}
+
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	if dbDriver == "cloudsql" {
+		var (
+			instanceConnectionName = os.Getenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
+		)
+
+		socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+		if !isSet {
+			socketDir = "/cloudsql"
+		}
+
+		conString = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUser, dbPass, socketDir, instanceConnectionName, dbName)
+
+	} else {
+		conString = "host=" + dbHost + " port=" + dbPort + " user=" + dbUser + " password=" + dbPass + " dbname=" + dbName + " sslmode=disable"
+		// conString = dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName
+	}
+
+	db, err = sql.Open("postgres", conString)
+	helpers.ErrorCheck(err)
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	err = db.Ping()
+	helpers.ErrorCheck(err)
+}
+func CreateCon() *sql.DB {
+	return db
+}
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/nikitamirzani323/go_wl_api/db"
+	"github.com/nikitamirzani323/go_wl_api/routes"
+)
+
+func main() {
+	db.Init()
+	app := routes.Init()
+	log.Fatal(app.Listen(":7073"))
 }
